c93b3e271ac551bce8cd7100466b3f453e302c62
sathwikreddy122 <venkatasathwik12604@gmail.com>
2026-07-04 15:31:41 +0530

    created project

---
+++ url_shorterner/src/main/java/com/example/url_shorterner/UrlShorternerApplication.java
@@ -0,0 +1,13 @@
+package com.example.url_shorterner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShorternerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShorternerApplication.class, args);
+	}
+
+}
---
+++ url_shorterner/src/test/java/com/example/url_shorterner/UrlShorternerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.url_shorterner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShorternerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
